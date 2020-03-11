d700ad40cb0345e7fe0f3a4d5e20d0569976c6b3
John Wells <molekewl2017@gmail.com>
2020-03-11 09:40:22 -0700

    add morgan middleware

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
+
+// global middleware
+server.use(morgain("dev")); // third party, needs to be npm installed
+server.use(express.json()); // built in middleware: no need to npm i
 
 server.use(express.json());
 
