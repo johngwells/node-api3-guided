e3f0dd8bd9a8c66c2f0c5a0c1c4ab9fec3430fc3
John Wells <molekewl2017@gmail.com>
2020-03-11 10:15:43 -0700

    add logger & helmet

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express'); // importing a CommonJS module
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 // global middleware
-server.use(morgain("dev")); // third party, needs to be npm installed
+server.use(morgan("dev")); // third party, needs to be npm installed
+server.use(helmet()); // third party, npm i
+server.use(logger);
 server.use(express.json()); // built in middleware: no need to npm i
-
-server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
 
@@ -21,5 +22,15 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function logger(req, res, next) {
+  // log information about the request to the console -> GET to /
+  const method = req.method;
+  const endpoint = req.originalUrl;
+
+  console.log(`${method} to ${endpoint}`);
+
+  next(); // moves the request to the next midddleware
+}
 
 module.exports = server;
